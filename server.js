54bca1bb8ea4e534429727dabedcf12448c6212a
Arbaaz <mr.khan4231@gmail.com>
2022-04-08 08:39:37 +0530

    logging-in-express-js-using-winston-and-morgan configured

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 // eslint-disable-next-line import/no-unresolved
-require("dotnev").config();
+require("dotenv").config();
+
+const morganMiddleware = require("./src/middlewares/morgan");
+// The morgan middleware does not need this.
+// This is for a manual log
+const logger = require("./src/utilities/logger");
 
 const app = express();
 const port = process.env.PORT;
 
+// Add the morgan middleware
+app.use(morganMiddleware);
+
 app.get("/test", (req, res) => {
+    logger.info("Checking the API status: Everything is OK");
     res.status(200).send(
         {
             message: "Hello Fellas!",
@@ -13,4 +22,4 @@ app.get("/test", (req, res) => {
     );
 });
 
-app.listen(port, () => console.log(`server is running on port:${port}`));
+app.listen(port, () => logger.info(`server is running on port:${port}`));
